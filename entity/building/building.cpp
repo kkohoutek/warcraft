861fe0417ee3bf39757dc1079dbfb7c480d61f29
KamilKohoutek <kamil008@seznam.cz>
2017-05-25 18:05:50 +0200

    Add files via upload

---
+++ entity/building/building.cpp
@@ -0,0 +1,5 @@
+#include "building.h"
+
+Building::Building() {
+
+}
--- entity/building/building.h
+++ entity/building/building.h
@@ -1,1 +1,30 @@
+#ifndef BUILDING_H
+#define BUILDING_H
 
+#include "entity/entity.h"
+//#include "entity/unit/unit.h"
+
+
+
+class Building : public Entity
+{
+public:
+    Building();
+
+    enum BuildingType {H_BARRACKS, H_FARM};
+
+    // Entity interface
+    void update() override;
+
+
+private:
+    QPixmap *buildAnimation;
+
+    QList<BuildingType> *unitsAvailable;
+    QList<BuildingType> *unitsTrained;
+
+    QTimer *trainingTimer;
+    QTimer *buildTimer;
+};
+
+#endif // BUILDING_H
